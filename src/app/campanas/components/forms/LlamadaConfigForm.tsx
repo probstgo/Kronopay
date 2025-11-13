7bb6282475760c53714ae1b635148d82ebd1e1cc
Santiago Alvarez <santiagoalvarezdelrio@gmail.com>
2025-11-13 10:18:31 -0300

    Ajuste: mejoras en LlamadaConfigForm
    - Optimizaciones de UX y validaciones
    - Mejoras en configuración de llamadas
    - Código más robusto y eficiente

--- src/app/campanas/components/forms/LlamadaConfigForm.tsx
+++ src/app/campanas/components/forms/LlamadaConfigForm.tsx
@@ -97,7 +97,7 @@ export function LlamadaConfigForm({ node, onSave }: LlamadaConfigFormProps) {
           >
             <option value="">Seleccionar agente</option>
             {agentes.map((agente) => (
-              <option key={agente.id} value={agente.id}>
+              <option key={agente.id} value={agente.agent_id}>
                 {agente.nombre}
               </option>
             ))}
